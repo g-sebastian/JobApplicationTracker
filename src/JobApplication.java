796ee5978f83c6669cdf08b9f895acd8f2af3928
Sebastian Greimel <sebastian.greimel@gmx.de>
2026-05-16 19:26:57 +0200

    JobApplication Basis fertig

--- src/JobApplication.java
+++ src/JobApplication.java
@@ -17,4 +17,45 @@ public class JobApplication {
         this.status = status;
         this.notes = notes;
     }
+
+    public void displayApplication() {
+        System.out.println("------------------------");
+        System.out.println("Company: " + company);
+        System.out.println("Position: " + position);
+        System.out.println("Location: " + location);
+        System.out.println("Job Link: " + jobLink);
+        System.out.println("Application Date: " + applicationDate);
+        System.out.println("Status: " + status);
+        System.out.println("Notes: " + notes);
+        System.out.println("------------------------");
+
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getJobLink() {
+        return jobLink;
+    }
+
+    public String getApplicationDate() {
+        return applicationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
 }
